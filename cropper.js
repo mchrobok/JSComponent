5cf7c1ce5cde4d59ff1d9d9d32a013b7ca8fb289
Mateusz Chrobok <mateusz.chrobok@gmail.com>
2013-08-14 09:43:23 +0200

    2 way communication by methods, not by url

--- cropper.js
+++ cropper.js
@@ -1,32 +1,31 @@
-var crop;
-$(document).ready(function () {
-  function ImageCropper(src) {
+var imageCropper = new ImageCropper ();
 
-    var cropper;
+function ImageCropper() {
 
-    jQuery(function ($) {
-      $('#image').Jcrop({
-        aspectRatio: 4 / 3
-      }, function () {
-        cropper = this;
-      });
+  var cropper;
+
+  this.aspectRatioChanged = function (combobox) {
+    $('#image').Jcrop({
+      aspectRatio: combobox.options[combobox.selectedIndex].value
     });
+  };
 
-    this.aspectRatioChanged = function (combobox) {
-      $('#image').Jcrop({
-        aspectRatio: combobox.options[combobox.selectedIndex].value
-      });
-    };
+  this.crop = function () {
+    var coords = cropper.tellSelect();
+    $('#height').text('height: ' + coords.h + ', width: ' + coords.w);
+  };
 
-    this.crop = function () {
-      var coords = cropper.tellSelect();
-      $('#height').text('height: ' + coords.h + ', width: ' + coords.w);
-    };
-
+  this.reload = function (src) {
     $('#image').attr('src', src);
+    $('#image').Jcrop({
+      aspectRatio: 4/3
+    }, function () {
+      cropper = this;
+    });
+  };
+};
 
-  }
-
-  crop = new ImageCropper('image.jpg');
-});
-
+loadImage = function (src, imageLoadedCallback) {
+  imageCropper.reload(src);
+  imageLoadedCallback();
+};
